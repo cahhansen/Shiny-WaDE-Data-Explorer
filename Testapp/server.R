ac3b920df11bcda4c8eb2fcb1ea2adbda0761d8b
cahhansen <carlyahansen@gmail.com>
2017-03-06 22:04:23 -0700

    Add options for different data types

--- Testapp/server.R
+++ Testapp/server.R
@@ -1,12 +1,14 @@
 # server.R
 library(ggplot2)
+library(XML)
+library(RColorBrewer)
 shinyServer(
   function(input, output) {
     
     
     output$plot <- renderPlot({
       #Fetch and parse data
-      xml.url=paste0('http://water.utah.gov/DWRE/WADE/v0.2/GetSummary/GetSummary.php?loctype=REPORTUNIT&loctxt=',input$reportingunit,'&orgid=utwre&reportid=',input$year,'_ConsumptiveUse&datatype=USE')
+      xml.url=paste0('http://water.utah.gov/DWRE/WADE/v0.2/GetSummary/GetSummary.php?loctype=REPORTUNIT&loctxt=',input$reportingunit,'&orgid=utwre&reportid=',input$year,'_',input$datatype,'&datatype=ALL')
       root= xmlRoot(xmlParse(xml.url))
       #Extract Report Summary
       reportsummary=root[["Organization"]][["Report"]][["ReportingUnit"]][["WaterUseSummary"]]
@@ -17,11 +19,28 @@ shinyServer(
       #Format into data.frame object
       wateruse_df=data.frame(t(waterusesummaryinfo),row.names=NULL,stringsAsFactors = FALSE)
       wateruse_df$AmountNumber=as.numeric(wateruse_df$AmountNumber)
-      wateruse_df$WaterUse=as.factor(waterusetype)
-      #barplot(height=wateruse_df$AmountNumber,names.arg=wateruse_df$WaterUse,main=paste0("Water Use by Sector in ",input$year))
-      ggplot(data=wateruse_df,environment = environment())+
-        geom_bar(aes(x=WaterUse,y=AmountNumber),stat="identity")+
-        theme_bw()+xlab("Sector")+ylab("Water Use in Gallons")
+      wateruse_df$WaterUseTypeName=as.factor(waterusetype)
+      if(input$datatype=="ConsumptiveUse"){
+        title="Comparison of Water Use by Sector"
+        ggplot(data=wateruse_df,environment = environment())+
+          geom_bar(aes(x=WaterUseTypeName,y=AmountNumber, fill=WaterUseTypeName),stat="identity")+
+          theme_bw()+scale_fill_brewer(palette="Paired")+
+          xlab("Sector")+ylab("Water Use (acre-feet/year)")+ggtitle(title)+
+          geom_text(aes(label = AmountNumber, x=WaterUseTypeName, y=AmountNumber),size = 5, position = position_stack(vjust = 0.5))+
+          theme(legend.position="none",plot.title = element_text(hjust = 0.5))
+      }else if(input$datatype=="Diversion"){
+      sourcetype=xmlSApply(reportsummary,function(x) xmlSApply(x[[3]][[2]],xmlValue))
+      wateruse_df$SourceTypeName=as.factor(sourcetype)
+        title="Comparison of Diversions by Sector"
+        ggplot(data=wateruse_df,environment = environment())+
+          geom_bar(aes(x=WaterUseTypeName,y=AmountNumber, fill=SourceTypeName),stat="identity")+
+          theme_bw()+scale_fill_brewer(palette="Paired", name="Source")+
+          xlab("Sector")+ylab("Water Use (acre-feet/year)")+ggtitle(title)+
+          geom_text(aes(label = AmountNumber, x=WaterUseTypeName, y=AmountNumber),size = 3, position = position_stack(vjust = 0.5))+
+          theme(legend.position="bottom",plot.title = element_text(hjust = 0.5))
+        
+      }
+      
       })
   }
 )
--- Testapp/ui.R
+++ Testapp/ui.R
@@ -1,23 +1,28 @@
 # ui.R
 
-shinyUI(fluidPage(
+shinyUI(fluidPage(theme="bootstrapspacelab.css",
   titlePanel("WaDE Data Exploration"),
   
   sidebarLayout(
     sidebarPanel(
-      helpText("Explore water consumption by sector."),
+      helpText("Explore water consumption by sector, as reported by the 
+               Utah Division of Water Resources."),
       
-      selectInput("year", 
+      selectInput(inputId = "year", 
                   label = "Choose a year to display:",
                   choices = as.character(seq(2000,2015,by=1)),
                   selected = "2010"),
-      selectInput("reportingunit",
-                  label = "Choose a reporting unit:",
+      selectInput(inputId = "reportingunit",
+                  label = "Choose a reporting unit (location):",
                   choices = c("04-01-10","03-01-01","02-01-09"),
-                  selected = "03-01-01")
-     # submitButton("Submit")
+                  selected = "03-01-01"),
+      selectInput(inputId = "datatype",
+                  label = "Choose a data type: ",
+                  choices = c("ConsumptiveUse","Diversion"))
     ),
     
-    mainPanel(plotOutput("plot"))
+    mainPanel(
+      plotOutput(outputId="plot"),height="400px")
   )
 ))
+
