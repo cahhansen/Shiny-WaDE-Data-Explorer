d324a32b3ccf5106e06cbff77c4a69444029a24f
cahhansen <carlyahansen@gmail.com>
2017-03-20 11:01:35 -0600

    Finalize example app for Utah

---
+++ UtahDataApp/server.R
@@ -0,0 +1,111 @@
+# server.R
+library(ggplot2)
+library(XML)
+library(RColorBrewer)
+shinyServer(
+  function(input, output) {
+    #CONSUMPTIVE USE###################################################################################################################################    
+    #Create reactive function that will fetch consumptive use data when the user changes the inputs (year or location)
+    CU_data <- reactive({
+      #Fetch and parse data
+      xml.urlCU=paste0('http://water.utah.gov/DWRE/WADE/v0.2/GetSummary/GetSummary.php?loctype=REPORTUNIT&loctxt=',input$reportingunit,'&orgid=utwre&reportid=',input$year,'_ConsumptiveUse&datatype=ALL')
+
+        CUroot= xmlRoot(xmlParse(xml.urlCU,useInternalNodes = TRUE))
+        #Extract Report Summary
+        CUreportsummary=CUroot[["Organization"]][["Report"]][["ReportingUnit"]][["WaterUseSummary"]]
+        #Water use Categories (e.g. Agricultural, Municipal/Industrial)
+        CUwaterusetype=xmlSApply(CUreportsummary,function(x) xmlSApply(x[[2]],xmlValue))
+        CU_df=data.frame(waterusetype=as.factor(CUwaterusetype),row.names=NULL,stringsAsFactors = FALSE)
+        n.uses=length(xmlToList(CUreportsummary))
+        #Get values for the use amount
+        for (i in seq(1,n.uses)){
+          CUsummaryinfo=xmlSApply(CUreportsummary[[i]],xmlValue)
+          CUamountsummary=xmlSApply(CUreportsummary[[i]][[3]],xmlValue)
+          if(is.null(CUreportsummary[[i]][[3]][["WaterUseAmount"]])){
+            CUamount=as.character(rep(NA,4))
+          }else{
+          CUamount=xmlSApply(CUreportsummary[[i]][[3]][["WaterUseAmount"]],xmlValue)
+          }
+        CU_df[i,"AmountNumber"]=as.numeric(CUamount[1])
+        }
+      return(CU_df)
+    })
+    
+    #Returns information about the method
+    xml.urlCUMethod=paste0('http://water.utah.gov/DWRE/WADE/v0.2/GetMethod/GetMethod.php?methodid=utwre&methodname=CONSUMPTIVE_USE')
+    CUmethodroot= xmlRoot(xmlParse(xml.urlCUMethod))
+    #Extract Report Summary
+    CUmethodsummary=CUmethodroot[["Organization"]][["Method"]]
+    CUmethodinfo=xmlSApply(CUmethodsummary,function(x) xmlSApply(x,xmlValue))
+    CUmethod_df=data.frame(t(CUmethodinfo),row.names=NULL,stringsAsFactors = FALSE)
+    
+    #Plots the results from the GetSummary call
+    output$CUplot <- renderPlot({
+      #Get data from reactive function (based on inputs of year and reporting unit)
+      CU_df=CU_data()
+      title="Comparison of Water Use by Sector"
+      ggplot(data=CU_df,environment = environment())+
+        geom_bar(aes(x=waterusetype,y=AmountNumber, fill=waterusetype),stat="identity")+
+        theme_bw()+scale_fill_brewer(palette="Paired")+
+        xlab("Sector")+ylab("Water Use (acre-feet/year)")+ggtitle(title)+
+        geom_text(aes(label = AmountNumber, x=waterusetype, y=AmountNumber),size = 5, position = position_stack(vjust = 0.5))+
+        theme(legend.position="none",plot.title = element_text(hjust = 0.5))
+    })
+    #Prints the method information
+    output$CUMethod <- renderText(
+        paste0("For more information about the methods used, see: ",CUmethod_df$MethodLinkText[[1]])
+      )
+    
+    #DIVERSION##########################################################################################################################################    
+    #Create reactive function that will fetch diversion data when the user changes the inputs (year or location)
+    Div_data <- reactive({
+      #Fetch and parse data
+      xml.urlDiv=paste0('http://water.utah.gov/DWRE/WADE/v0.2/GetSummary/GetSummary.php?loctype=REPORTUNIT&loctxt=',input$reportingunit,'&orgid=utwre&reportid=',input$year,'_Diversion&datatype=ALL')
+        Divroot= xmlRoot(xmlParse(xml.urlDiv))
+        #Extract Report Summary
+        Divreportsummary=Divroot[["Organization"]][["Report"]][["ReportingUnit"]][["WaterUseSummary"]]
+        #Water use Categories (e.g. Agricultural, Municipal/Industrial)
+        Divwaterusetype=xmlSApply(Divreportsummary,function(x) xmlSApply(x[[2]],xmlValue))
+        Div_df=data.frame(waterusetype=as.factor(Divwaterusetype),row.names=NULL,stringsAsFactors = FALSE)
+        n.uses=length(xmlToList(Divreportsummary))
+        #Get values for the use amount
+        for (i in seq(1,n.uses)){
+          Divsummaryinfo=xmlSApply(Divreportsummary[[i]],xmlValue)
+          Divamountsummary=xmlSApply(Divreportsummary[[i]][[3]],xmlValue)
+          Div_df[i,"SourceType"]=Divamountsummary[2]
+          if(is.null(Divreportsummary[[i]][[3]][["WaterUseAmount"]])){
+            Divamount=as.character(rep(NA,4))
+          }else{
+            Divamount=xmlSApply(Divreportsummary[[i]][[3]][["WaterUseAmount"]],xmlValue)
+          }
+          Div_df[i,"AmountNumber"]=as.numeric(Divamount[1])
+        }
+      return(Div_df)
+    })
+    
+    #Returns information about the method
+    xml.urlDivMethod=paste0('http://water.utah.gov/DWRE/WADE/v0.2/GetMethod/GetMethod.php?methodid=utwre&methodname=DIVERSION')
+    Divmethodroot= xmlRoot(xmlParse(xml.urlDivMethod))
+    #Extract Report Summary
+    Divmethodsummary=Divmethodroot[["Organization"]][["Method"]]
+    Divmethodinfo=xmlSApply(Divmethodsummary,function(x) xmlSApply(x,xmlValue))
+    Divmethod_df=data.frame(t(Divmethodinfo),row.names=NULL,stringsAsFactors = FALSE)
+    
+    #Plots results from the GetSummary call
+    output$Divplot <- renderPlot({
+      Div_df=Div_data()
+      title="Comparison of Diversions by Sector"
+      ggplot(data=Div_df,environment = environment())+
+        geom_bar(aes(x=waterusetype,y=AmountNumber, fill=SourceType),stat="identity")+
+        theme_bw()+scale_fill_brewer(palette="Paired", name="Source")+
+        xlab("Sector")+ylab("Diversions (acre-feet/year)")+ggtitle(title)+
+        geom_text(aes(label = AmountNumber, x=waterusetype, y=AmountNumber),size = 5, position = position_stack(vjust = 0.5))+
+        theme(legend.position="bottom",plot.title = element_text(hjust = 0.5))
+    })
+    
+    #Prints the method information
+    output$DivMethod <- renderText(
+      paste0("For more information about the methods used, see: ",Divmethod_df$MethodLinkText[[1]])
+      )
+  }
+)
