206fcd30a9fbf16d3f957ca655f354c157ab182b
cahhansen <carlyahansen@gmail.com>
2017-04-17 15:33:27 -0600

    Update Wyoming app

--- WyomingDataApp/server.R
+++ WyomingDataApp/server.R
@@ -3,34 +3,66 @@ library(ggplot2)
 library(XML)
 library(RColorBrewer)
 shinyServer(
-  function(input, output) {
-    #CONSUMPTIVE USE###################################################################################################################################    
+  function(input, output, session) {
+    outVar = reactive({
+      statedata = xmlRoot(xmlParse('http://www.westernstateswater.org/Wyoming/WADE/v0.2/GetCatalog/GetCatalog_GetAll.php?orgid=WYWDC'))
+      n.reports=length(xmlToList(statedata))
+      RU_df=data.frame(RowIndex=seq(1,n.reports),row.names=NULL,stringsAsFactors = FALSE)
+      for(i in seq(1,n.reports)){
+        reporttype=xmlSApply(statedata[[i]][["DataType"]],xmlValue)
+        if(reporttype=="USE"){
+          reportingunit_use=xmlSApply(statedata[[i]][["ReportUnitIdentifier"]],xmlValue)
+          reportingunitname_use=xmlSApply(statedata[[i]][["ReportUnitName"]],xmlValue)
+        }else{
+          reportingunit_use=NA
+          reportingunitname_use=NA
+        }
+        RU_df[i,"ReportingUnit"]=as.numeric(reportingunit_use[1])
+        RU_df[i,"ReportingUnitName"]=as.character(reportingunitname_use[1])
+      }
+      
+      return(RU_df)
+    })
+    observe({
+      RU_df=outVar()
+      RUNames=RU_df[!is.na(RU_df$ReportingUnitName),"ReportingUnitName"]
+      
+      updateSelectInput(session, "reportingunit",
+                        choices = RUNames)
+      })
+    
+    #WATER USE###################################################################################################################################    
     #Create reactive function that will fetch consumptive use data when the user changes the inputs (year or location)
     CU_data <- reactive({
+      RU_df=outVar()
+      RU_dfSub=RU_df[!is.na(RU_df$ReportingUnitName),]
+      RUNumber=RU_dfSub[(RU_dfSub$ReportingUnitName==input$reportingunit),"ReportingUnit"]
       #Fetch and parse data
-      xml.urlCU=paste0('http://www.westernstateswater.org/Wyoming/WADE/v0.2/GetSummary/GetSummary.php?loctype=REPORTUNIT&loctxt=',input$reportingunit,'&orgid=WYWDC&reportid=2014&datatype=USE')
+      xml.urlCU=paste0('http://www.westernstateswater.org/Wyoming/WADE/v0.2/GetSummary/GetSummary.php?loctype=REPORTUNIT&loctxt=',RUNumber,'&orgid=WYWDC&reportid=2014&datatype=USE')
       CUroot= xmlRoot(xmlParse(xml.urlCU,useInternalNodes = TRUE))
       #Extract Report Summary
       CUreportsummary=CUroot[["Organization"]][["Report"]][["ReportingUnit"]][["WaterUseSummary"]]
       #Water use Categories (e.g. Agricultural, Municipal/Industrial)
-      CUwaterusetype=xmlSApply(CUreportsummary,function(x) xmlSApply(x[[3]],xmlValue))
+      CUwaterusetype=xmlSApply(CUreportsummary,function(x) xmlSApply(x[["WaterUseTypeName"]],xmlValue))
       CU_df=data.frame(Sector=as.factor(CUwaterusetype),row.names=NULL,stringsAsFactors = FALSE)
       n.uses=length(xmlToList(CUreportsummary))
         #Get values for the use amount
         for (i in seq(1,n.uses)){
-          CUamountsummary=xmlSApply(CUreportsummary[[i]][[4]],xmlValue)
-          if(is.null(CUreportsummary[[i]][[4]][["WaterUseAmount"]])){
+          CUamountsummary=xmlSApply(CUreportsummary[[i]][["WaterUseAmountSummary"]],xmlValue)
+          if(is.null(CUreportsummary[[i]][["WaterUseAmountSummary"]][["WaterUseAmount"]])){
             CUamount=as.character(rep(NA,4))
           }else{
-          CUamount=xmlSApply(CUreportsummary[[i]][[4]][["WaterUseAmount"]],xmlValue)
+          CUamount=xmlSApply(CUreportsummary[[i]][["WaterUseAmountSummary"]][["WaterUseAmount"]],xmlValue)
           CUsource=CUamountsummary[2]
           }
         CU_df[i,"Amount"]=as.numeric(CUamount[1])
         CU_df[i,"Source"]=CUsource
         }
       return(CU_df)
+      
+      
     })
-    CU_data()
+
     
     #Returns information about the method
     xml.urlCUMethod=paste0('http://www.westernstateswater.org/Wyoming/WADE/v0.2/GetMethod/GetMethod.php?methodid=WYWDC&methodname=Green%20River%20Basin%20Report%20Consumptive%20Use%20Estimation')
@@ -55,10 +87,10 @@ shinyServer(
     output$CUmethod <- renderText(
         paste0("For more information about the methods used, see: ",CUmethod_df$MethodLinkText[[1]])
       )
-    
-    #Lists water use information in a table
-    CU_df=CU_data()
-    output$table <- renderTable(CU_df)
-    
+
+    #List amounts in table
+    output$table <- renderTable({
+      CU_df=CU_data()
+    },bordered=TRUE, striped=TRUE)
   }
 )
--- WyomingDataApp/ui.R
+++ WyomingDataApp/ui.R
@@ -1,5 +1,4 @@
 # ui.R
-reportingunits=seq(1,7,1) 
 
 shinyUI(fluidPage(theme="bootstrapdarkly.css",
   titlePanel("Wyoming Water Use Data Exploration"),
@@ -7,20 +6,16 @@ shinyUI(fluidPage(theme="bootstrapdarkly.css",
   sidebarLayout(
     sidebarPanel(
       helpText("Explore water use by sector."),
-      
       selectInput(inputId = "reportingunit",
                   label = "Select a reporting unit (location):",
-                  choices = reportingunits,
-                  selected = "1")
+                  choices = c("Fetching Reporting Units"),
+                  selected = "Fetching Reporting Units")
     ),
     
     mainPanel(
       plotOutput(outputId="CUplot"),height="400px",
+      tableOutput("table"),
       textOutput(outputId="CUmethod")
-      ),
-    
-    fluidRow(
-      tableOutput("table")
     )
   )
 ))
